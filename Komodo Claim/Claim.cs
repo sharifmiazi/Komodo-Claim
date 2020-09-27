11cc13d4e05890540929e1b54a82a6c91b459d2f
sharifmiazi <62771733+sharifmiazi@users.noreply.github.com>
2020-09-27 18:55:07 -0400

    Add project files.

---
+++ Komodo Claim/Claim.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Claim
+{
+    public enum claimTypes {Car, Home, Theft}
+    class Claim
+    {
+        //properties 
+        public int ClaimID { get; set; }
+        public int ClaimTypes { get; set; }
+        public string ClaimDescription { get; set; }
+        public double ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid
+        {
+            get
+            {
+                int numOfDays = ((TimeSpan)(DateOfClaim - DateOfIncident)).Days;
+                Console.WriteLine(numOfDays);
+                if 
+                    (numOfDays > 30)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+
+            }
+        }
+
+        
+        //constructors
+        public Claim(int claimID, int claimTypes, string claimDescription, double claimAmount, DateTime dateOfClaim, DateTime dateOfIncident)
+        {
+            ClaimID = claimID;
+            ClaimTypes = claimTypes;
+            ClaimDescription = claimDescription;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+        }
+    }
+}
---
+++ Komodo Claim/Claim_Repo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komodo_Claim
+{
+    class Claim_Repo
+    {
+        private readonly List<Claim> _claimDictionary = new List<Claim>();
+        private readonly Queue<Claim> _claimQue = new Queue<Claim>();
+        
+        //create
+        public void AddClaimToDictionary(Claim list)
+        {
+            _claimQue.Enqueue(list);
+        }
+        //Read
+        public List<Claim> GetClaims()
+        {
+            return _claimDictionary;
+        }
+        public Queue<Claim> GetClaimsQue()
+        {
+            return _claimQue;
+        }
+    }
+}
